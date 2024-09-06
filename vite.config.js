071008b90acceecbbb55261150bc41b3c2ebf122
Elazar Pimentel <elazar.pimentel@gmail.com>
2024-09-06 07:59:55 -0300

    Arreglo: Vercel no toma estilos. Intentando solución en configuración

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Ensure that assets like CSS and images are correctly referenced in the build
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
