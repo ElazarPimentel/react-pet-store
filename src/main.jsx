20b268a5721a74228df03edbbbeee745d6825ea5
Elazar Pimentel <elazar.pimentel@gmail.com>
2024-09-06 08:24:15 -0300

    Arreglo: Cambiar shared.module.css a shared.css y asegurar manejo correcto de módulos CSS

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.jsx';
-import './shared.module.css'; 
+import './shared.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
